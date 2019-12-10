fa364627b191ea53e4455682f74b40ead9ddb84e
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-12-11 01:08:52 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopNavBar from "./components/TopNavBar/TopNavBar";
+function App() {
+  return (
+    <React.Fragment>
+      <TopNavBar />
+      <h1>Hello</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "bootstrap";
+import "./scss/style.scss";
+
+class TopNavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>I am a TopNavBar</h1>
+      </React.Fragment>
+    );
+  }
+}
+export default TopNavBar;
