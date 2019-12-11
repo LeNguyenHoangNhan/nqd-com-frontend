4563eb7c0911e4b582bb5ec15857681fd6267e5e
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-12-11 19:18:32 +0700

    LearnReact!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import TopNavBar from "./components/TopNavBar/TopNavBar";
 function App() {
-  return (
-    <React.Fragment>
-      <TopNavBar />
-      <h1>Hello</h1>
-    </React.Fragment>
-  );
+  return <React.Fragment></React.Fragment>;
 }
 
 export default App;
