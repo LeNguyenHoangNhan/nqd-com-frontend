52e753f36cdfe72765b59125da0ed5906b85b4dd
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-12-11 23:49:29 +0700

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import LandingPageBody from "./components/LandingPageBody/LandingPageBody";
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <LandingPageBody></LandingPageBody>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/LandingPageBody/LandingPageBody.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as EnquireJS from "enquire.js";
+const LandingPageBody = () => (
+  <React.Fragment>
+    <h2>Hello</h2>
+  </React.Fragment>
+);
+export default LandingPageBody;
