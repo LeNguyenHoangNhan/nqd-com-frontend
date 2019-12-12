b9b571ad3b438debd07348ce555befc7bde21968
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-12-12 10:12:12 +0700

    Minor Changes

--- src/components/LandingPageBody/LandingPageBody.tsx
+++ src/components/LandingPageBody/LandingPageBody.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as EnquireJS from "enquire.js";
-const LandingPageBody = () => (
+const LandingPageBody = (): JSX.Element => (
   <React.Fragment>
     <h2>Hello</h2>
   </React.Fragment>
